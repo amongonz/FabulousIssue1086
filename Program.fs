96f8ca220619961c25cd45ba8b5ea6ba0a5f3463
Adrián Montesinos González <amontesinosgonz@gmail.com>
2024-11-05 01:46:41 +0100

    Add initial files

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+open Avalonia
+open Avalonia.Themes.Simple
+
+open Fabulous.Avalonia
+open type Fabulous.Avalonia.View
+
+let view () =
+    DesktopApplication(Window(VStack() { () }))
+
+let program = Program.stateless view
+
+[<EntryPoint; STAThread>]
+let main args =
+    FabulousAppBuilder
+        .Configure(SimpleTheme, program)
+        .UsePlatformDetect()
+        .StartWithClassicDesktopLifetime(args)
